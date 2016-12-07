4f49ef84330af8d3c45c3fc94a111244f7d5536f
TruthIsNear <kraskov.den@gmail.com>
2016-12-07 07:49:43 +0300

    changed task5_7_1

--- task5_7_1/main.cpp
+++ task5_7_1/main.cpp
@@ -9,7 +9,7 @@ void DeleteElement(int* arr, int length, int pos)
 
 int main()
 {
-    int *b, *b1, *c, *c1, *a, n=0, n1=0, m=0, m1=0, p=1, count=0, count1=0, max=0;
+    int *b, *c, *a, n=0, n1=0, m=0, m1=0, p=1, count=0, max=0;
     cout<<"The amount of elements in the array b="; cin>>n;
     b=new int[n];
     for (int i=0;i<n;i++)
@@ -27,15 +27,6 @@ int main()
     }
     cout<<"p="<<p<<"\n";
     cout<<"max="<<max<<"\n";
-    b1=new int[n1];
-    for (int i=0;i<n;i++)
-    {
-        if (b[i]>=0)
-        {
-            b1[count]=b[i];
-            count++;
-        }
-    }
     cout<<"The amount of elements in the array c="; cin>>m;
     c=new int[m];
     for (int i=0;i<m;i++)
@@ -43,18 +34,15 @@ int main()
         cout<<"C["<<i<<"]="; cin>>c[i];
         if (c[i]>=0) m1++;
     }
-    c1=new int[m1];
-    for (int i=0;i<m;i++)
-    {
-        if (c[i]>=0)
-        {
-            c1[count1]=c[i];
-            count1++;
-        }
-    }
     a=new int[n1+m1];
-    for (int i=0;i<n1;i++) a[i]=b1[i];
-    for (int i=n1;i<n1+m1;i++) a[i]=c1[i-n1];
+    for (int i=0;i<n;i++)
+    {
+        if (b[i]>=0) a[count++]=b[i];
+    }
+    for (int i=0;i<n1+m;i++)
+    {
+        if (c[i]>=0) a[count++]=c[i];
+    }
     cout<<"Array A:\n";
     for (int i=0; i<n1+m1; i++) cout<<a[i]<<" ";
     for (int i=0; i<n1+m1; i++)
@@ -68,9 +56,7 @@ int main()
     cout<<"\nNew array A:\n";
     for (int i=0; i<n1+m1-1; i++) cout<<a[i]<<" ";
     delete [] b;
-    delete [] b1;
     delete [] c;
-    delete [] c1;
     delete [] a;
     return 0;
 }
